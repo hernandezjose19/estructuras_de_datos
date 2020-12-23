169baa1129ae7396a500aa24b8d2cd7f2944aac0
Jose Hernandez <antonioaljorna@gmail.com>
2020-12-23 18:41:08 -0300

    Se corrige logica de pilas

--- pilas.py
+++ pilas.py
@@ -7,7 +7,7 @@ class Pila():
 
     def push(self, elemen):
 
-        self.lista.append(elemen)
+        self.lista.insert(0, elemen)
         print(f"Se anadio el elemnto {elemen}")
         print(self.lista)
 
@@ -15,13 +15,14 @@ class Pila():
     def pop(self):
 
         print(self.lista)
-        del self.lista[-1]
+        del self.lista[0]
         print(self.lista)
 
 
     def peek(self):
 
-        print(self.lista[-1])
+
+        print(self.lista[0])
 
 
 
