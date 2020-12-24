a12d61754b90f3609b524661e9e308d50fd33cdb
Jose Hernandez <antonioaljorna@gmail.com>
2020-12-23 21:05:39 -0300

    Cola, estructura de dato, se anade cambio

--- cola.py
+++ cola.py
@@ -17,10 +17,3 @@ class Cola():
     def front(self):
         print(self.lista[0])
 
-
-mi_cola = Cola()
-mi_cola.queue(50)
-mi_cola.queue(20)
-mi_cola.queue(89)
-mi_cola.front()
-mi_cola.dequeue()
